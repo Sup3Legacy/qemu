1d79bff59e0240c064d082202261fda0e8ce93a2
Constantin Gierczak--Galle <git.cst1@mailbox.org>
2024-05-01 11:39:59 +0200

    Work on cache data structure definition and config interface

--- include/hw/misc/cache_sim.h
+++ include/hw/misc/cache_sim.h
@@ -22,10 +22,13 @@ typedef enum {
 
 typedef struct {
     uint64_t tag;
+    // Whether there is a valid cache line stored here
     bool is_valid;
+    // Whether the cache line has been modified but not flushed to the lower
+    // level cache/memory
+    bool is_dirty;
 
-    // Maybe we can allocate all the block memory in one go and give
-    // intermediate pointers.
+    // This points inside the CacheUnit->cache_memory
     // Length is (block_size)
     char *data;
 } Block;
@@ -39,10 +42,18 @@ typedef struct {
 typedef struct {
     // Length is (size / (assoc * block_size))
     Set *sets;
+
+    // Array holding the cache line data
+    // TODO: alignment?
+    char *cache_memory;
+
     // All three have to be powers of two
     uint64_t size;
     uint8_t assoc;
     uint32_t block_size;
+
+    // Cache policies
+    ReplacementPolicy rp;
 
     // Derived metrics
     uint64_t number_of_sets;
@@ -57,7 +68,44 @@ typedef struct {
     CacheUnit il1;
     CacheUnit dl1;
     CacheUnit l2;
+    CacheUnit l3;
+
+    // Write policy
+    // INFO: My guess is, this has to be the same accross all cache levels, so
+    // it makes more sesn to put it in here.
+    // WARN: Same thing for the replacement policy? Probably
+    WritePolicy wp;
 } CPUCache;
+
+// Structure holding the cache model configuration requested from the guest
+// kernel
+typedef struct {
+    struct {
+        bool enable;
+        uint64_t size;
+        uint8_t assoc;
+        uint32_t block_size;
+    } il1;
+    struct {
+        bool enable;
+        uint64_t size;
+        uint8_t assoc;
+        uint32_t block_size;
+    } dl1;
+    struct {
+        bool enable;
+        uint64_t size;
+        uint8_t assoc;
+        uint32_t block_size;
+    } l2;
+    struct {
+        bool enable;
+        uint64_t size;
+        uint8_t assoc;
+        uint32_t block_size;
+    } l3;
+    WritePolicy wp;
+} RequestedCaches;
 
 bool find_in_cache(CacheUnit *cache, uint64_t address);
 

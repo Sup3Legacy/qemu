ce85523592113888fff3c815d89f6b380f41821c
Constantin Gierczak--Galle <git.cst1@mailbox.org>
2024-05-07 16:50:10 +0200

    Barebones RAM topology config struct

--- include/hw/misc/mem_controller.h
+++ include/hw/misc/mem_controller.h
@@ -4,5 +4,25 @@
 #include <inttypes.h>
 #include "qom/object.h"
 
+typedef struct {
+    uint64_t size;
+
+    uint8_t channels;
+    uint8_t ranks;
+    uint8_t groups;
+    uint8_t banks;
+    uint32_t rows;
+
+    // NOTE: derive width of column?
+} MemTopology;
+
+/* 
+ * DRamsim provides a way to configure the linear-to-topology mapping
+ * in such a way: "ro ch ra ba bg co"
+ * i.e. row.channel.rank.bank.group.column
+ * https://github.com/umd-memsys/DRAMsim3/blob/29817593b3389f1337235d63cac515024ab8fd6e/configs/DDR3_4Gb_x4_1866.ini#L57
+ *
+ */
+
 #endif
 

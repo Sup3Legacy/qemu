dc63e55916a3a9f8a025cb91e9607077a830b92e
Constantin Gierczak--Galle <git.cst1@mailbox.org>
2024-05-10 16:06:39 +0200

    Add mem backend barebones files

---
+++ include/hw/misc/mem_backend.h
@@ -0,0 +1,9 @@
+#ifndef HW_MEM_BACKEND_H
+#define HW_MEM_BACKEND_H
+
+#include <inttypes.h>
+#include "qom/object.h"
+
+#endif
+
+
--- include/hw/misc/mem_controller.h
+++ include/hw/misc/mem_controller.h
@@ -14,7 +14,19 @@ typedef struct {
     uint32_t rows;
 
     // NOTE: derive width of column?
+
+    // NOTE: Derived log2s
+    uint8_t channels_log2;
+    uint8_t ranks_log2;
+    uint8_t groups_log2;
+    uint8_t banks_log2;
+    uint8_t rows_log2;
 } MemTopology;
+
+typedef struct {
+    MemTopology topology;
+
+} MemController;
 
 /* 
  * DRamsim provides a way to configure the linear-to-topology mapping

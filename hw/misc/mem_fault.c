b770798dc00da0ff0ae516385cc5124fdb766852
Constantin Gierczak--Galle <git.cst1@mailbox.org>
2024-05-10 16:08:29 +0200

    Add barebones memory fault files

---
+++ hw/misc/mem_fault.c
@@ -0,0 +1,5 @@
+#include "qemu/osdep.h"
+#include "qapi/error.h" /* provides error_fatal() handler */
+#include "hw/sysbus.h" /* provides all sysbus registering func */
+#include "hw/misc/mem_fault.h"
+
---
+++ include/hw/misc/mem_fault.h
@@ -0,0 +1,8 @@
+#ifndef HW_MEM_FAULT_H
+#define HW_MEM_FAULT_H
+
+#include <inttypes.h>
+#include "qom/object.h"
+
+#endif
+

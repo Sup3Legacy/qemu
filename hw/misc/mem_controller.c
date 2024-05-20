635cf214db197461d20cd369f0c8e84eb1956057
Constantin Gierczak--Galle <git.cst1@mailbox.org>
2024-05-21 00:39:43 +0200

    Compute all decoding offsets and masks

--- hw/misc/mem_controller.c
+++ hw/misc/mem_controller.c
@@ -3,3 +3,10 @@
 #include "hw/sysbus.h" /* provides all sysbus registering func */
 #include "hw/misc/mem_controller.h"
 
+// NOTE: 
+// in such a way: "ro ch ra ba bg co"
+
+// CONTRACT: Assumes `val` is within bounds ([0; `controller->topology.size`[).
+static void address_to_coords(MemController *controller, uint64_t val, MemCoords *coords) {
+
+}
--- include/hw/misc/mem_controller.h
+++ include/hw/misc/mem_controller.h
@@ -15,6 +15,7 @@ typedef struct {
     uint32_t rows;
 
     // NOTE: derive width of column?
+    uint32_t column_width;
 
     // NOTE: Derived log2s
     uint8_t channels_log2;
@@ -22,12 +23,40 @@ typedef struct {
     uint8_t groups_log2;
     uint8_t banks_log2;
     uint8_t rows_log2;
+    uint8_t column_width_log2;
 } MemTopology;
 
 typedef struct {
+    uint8_t channel_off;
+    uint8_t rank_off;
+    uint8_t group_off;
+    uint8_t bank_off;
+    uint8_t row_off;
+    uint8_t column_off;
+
+    uint32_t channel_mask;
+    uint32_t rank_mask;
+    uint32_t group_mask;
+    uint32_t bank_mask;
+    uint32_t row_mask;
+    uint32_t column_mask;
+} MemTopologyOffsets;
+
+typedef struct {
     MemTopology topology;
+    MemTopologyOffsets offsets;
 
 } MemController;
+
+typedef struct {
+    uint8_t channel;
+    uint8_t rank;
+    uint8_t group;
+    uint8_t bank;
+    uint32_t row;
+    uint32_t column;
+    // TODO: what next?
+} MemCoords;
 
 // FIXME: Already defined in cache_sim.h, we should merge these implementations
 static inline uint8_t log2i(uint64_t x) {
@@ -40,7 +69,35 @@ static void fill_log2s(MemTopology *topo) {
     topo->ranks_log2 = log2i(topo->ranks);
     topo->groups_log2 = log2i(topo->groups);
     topo->banks_log2 = log2i(topo->banks);
-    topo->tows_log2 = log2i(topo->rows);
+    topo->rows_log2 = log2i(topo->rows);
+    topo->column_width_log2 = log2i(topo->column_width);
+}
+
+// CONTRACT: assumes `topo`'s log2s have been filled
+static void fill_offsets(MemTopologyOffsets *offsets, MemTopology *topo) {
+    int8_t offset = 0;
+
+    // Compute all offsets
+    // NOTE: Currently hardcoded as: "ro ch ra ba bg co"
+    offsets->column_off = offset;
+    offset += topo->column_width_log2;
+    offsets->group_off = offset;
+    offset += topo->groups_log2;
+    offsets->bank_off = offset;
+    offset += topo->banks_log2;
+    offsets->rank_off = offset;
+    offset += topo->ranks_log2;
+    offsets->channel_off = offset;
+    offset += topo->channels_log2;
+    offsets->row_off = offset;
+
+    // Compute all masks
+    offsets->channel_mask = (1 << topo->channels_log2) - 1;
+    offsets->rank_mask = (1 << topo->ranks_log2) - 1;
+    offsets->group_mask = (1 << topo->groups_log2) - 1;
+    offsets->bank_mask = (1 << topo->banks_log2) - 1;
+    offsets->row_mask = (1 << topo->rows_log2) - 1;
+    offsets->column_mask = (1 << topo->columns_log2) - 1;
 }
 
 /* 
